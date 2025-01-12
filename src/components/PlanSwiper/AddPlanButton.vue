8a4130405894378ddaf1e50e62b62cdf0a4c6610
xinghe <90626136+caibimiaokong@users.noreply.github.com>
2025-01-12 22:34:52 +0800

    The new rate component allows switching between color and white tomato clocks

--- src/components/PlanSwiper/AddPlanButton.vue
+++ src/components/PlanSwiper/AddPlanButton.vue
@@ -1,15 +1,24 @@
 <script lang="ts" setup>
-import { ref,defineComponent } from 'vue';
+import { ref } from 'vue';
+
 import addLightImage from '@/assets/add_light.svg';
-import { Icon } from 'ant-design-vue';
+import WhiteTomatoIcon from "@/assets/white_clock.svg";
+import ColorTomatoIcon from "@/assets/red_clock.svg";
+
 import RangeButton from './RangeButton.vue'; 
+
 import { usePlanerStore } from '@/stores/planStore';
+
+
 
 const modalVisible = ref<boolean>(false);
 const planStore = usePlanerStore();
 const taskValue = ref<string>('');
 const isLoop = ref<boolean>(false);
-const pomodoroCount = ref<number>(1);
+const pomodoroCount = ref<number>(0);//current pomodoro count
+const hoverIndex = ref(0); // mouse hover index
+const whiteIcon = WhiteTomatoIcon;
+const coloredIcon = ColorTomatoIcon;
 </script>
 
 <template>
@@ -22,22 +31,44 @@ const pomodoroCount = ref<number>(1);
       centered
       @ok="modalVisible = false"
     >
-      <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 40px;">
-        <div style="display: flex; gap: 10px; margin-bottom: 10px;">
-          <a-input v-model:value="taskValue" placeholder="把事情记下来" />
-          <RangeButton/>
-        </div>
-        <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
-          <a-checkbox v-model:checked="isLoop">循环</a-checkbox>
-          <a-rate v-model:value="pomodoroCount" :count="4">
-          </a-rate>
-        </div>
-        
+    <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 40px;">
+      <div style="display: flex; gap: 10px; margin-bottom: 10px;">
+        <a-input v-model:value="taskValue" placeholder="把事情记下来" />
+        <RangeButton/>
       </div>
+      <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+        <a-checkbox v-model:checked="isLoop">循环</a-checkbox>
+        <div class="rate-container">
+          <div
+            v-for="index in 4"
+            :key="index"
+            class="rate-item"
+            @click="pomodoroCount = index"
+            @mouseenter="hoverIndex = index"
+            @mouseleave="hoverIndex = 0"
+          >
+            <img
+              :src="index <= (hoverIndex || pomodoroCount) ? coloredIcon : whiteIcon"
+              class="tomato-icon"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     </a-modal>
   </div>
 </template>
 
 
-
+<style scoped>
+.rate-container {
+  display: flex;
+  gap: 8px;
+}
+.tomato-icon {
+  width: 22px;
+  height: 22px;
+  cursor: pointer;
+}
+</style>
 
